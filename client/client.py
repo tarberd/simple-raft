ee54d852f4299da34f6661220de512fca8cc3bc6
tarberd <bernardo.mferrari@gmail.com>
2018-11-04 22:09:47 +0000

    change to asyncio and sockets

--- client/client.py
+++ client/client.py
@@ -1,15 +1,1 @@
-import requests
-
-server_list = [
-    'http://alce:8080/',
-    'http://baleia:8080/',
-    'http://camelo:8080/',
-    'http://doninha:8080/'
-]
-
-dict_to_send = {'value': 5}
-
-for server_url in server_list:
-    print('send post to: ' + server_url)
-    response = requests.post(server_url, json = dict_to_send)
-    print('response: ' + response.text)
+import socket
--- server/server.py
+++ server/server.py
@@ -1,15 +1,72 @@
-from flask import Flask, request, jsonify
+import socket
+import sys
+import time
+import asyncio
 
+def serve(connection):
+    print('Start serving')
+    while True:
+        connection.send(str.encode('heartbeat\n'))
+        time.sleep(5)
 
-app = Flask(__name__)
+def receive(connection):
+    print('Start receiving')
+    while True:
+        data = connection.recv(2048)
+        if not data:
+            break
+        print('received:', data.decode('utf-8'))
 
-@app.route('/', methods=['GET', 'POST'])
-def hello_world():
-    if request.method == 'POST':
-        value = request.json['value']
-        return 'value: ' + str(value)
-    else:
-        return 'value: ' + str(value)
+server_hostname_list = ['alce', 'baleia']
 
-if __name__ == '__main__':
-      app.run(host='0.0.0.0', port=8080)
+server_hostname_to_ip = {}
+server_ip_to_hostname = {}
+
+for host in server_hostname_list:
+    server_hostname_to_ip[host] = socket.gethostbyname(host)
+    server_ip_to_hostname[socket.gethostbyname(host)] = host
+
+print(server_hostname_to_ip)
+
+my_ip_address = socket.gethostbyname(socket.gethostname())
+
+print('I am:', server_ip_to_hostname[my_ip_address])
+print('I am at:', my_ip_address)
+
+del server_ip_to_hostname[my_ip_address]
+
+#  print('start')
+#  while True:
+#      their soc  = 0
+#      start_new_thread(connect_to, ())
+#
+#      connection, address = sock.accept()
+#      print('connected to: ' + address[0] + ':' + str(address[1]))
+#
+#      start_new_thread(serve, (connection,))
+#      start_new_thread(receive, (connection,))
+
+async def handle_echo(reader, writer):
+    data = await reader.read(100)
+    message = data.decode()
+    addr = writer.get_extra_info('peername')
+
+    print(f"Received {message!r} from {addr!r}")
+
+    print(f"Send: {message!r}")
+    writer.write(data)
+    await writer.drain()
+
+    print("Close the connection")
+    writer.close()
+
+async def main():
+    server = await asyncio.start_server(handle_echo, '0.0.0.0', 5555)
+
+    addr = server.sockets[0].getsockname()
+    print(f'Serving on {addr}')
+
+    async with server:
+        await server.serve_forever()
+
+asyncio.run(main())
